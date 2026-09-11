e70fcbd5c8a85bd92bfef8bbaa9ca9f7c49f1535
Jonas Lacandola <jonaslacandola07@gmail.com>
2026-09-12 01:03:36 +0800

    redesign(admin): load final control unification layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import "./public-modal-layer.css";
 import "./admin-control.css";
 import "./admin-editor-control.css";
 import "./admin-control-fixes.css";
+import "./admin-completion.css";
 import { cn } from "@/lib/utils";
 import { siteConfig } from "@/lib/site-config";
 import { ThemeProvider } from "@/components/layout/theme-provider";
