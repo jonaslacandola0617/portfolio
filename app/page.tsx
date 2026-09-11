38498e6e2371d6f6f7896406deb0acb509a8c590
Jonas Lacandola <jonaslacandola07@gmail.com>
2026-09-12 01:37:27 +0800

    refactor(home): render only project-managed showcase slots

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { JsonLd } from "@/components/shared/json-ld";
 import { siteConfig } from "@/lib/site-config";
 import { getAllProjects, getAllArticles, getAllLabs } from "@/lib/content";
 import { getAboutPage } from "@/lib/db/queries/about";
+import { getHomepageShowcaseProjectIds } from "@/lib/db/queries/homepage";
 import { getSiteSettings } from "@/lib/db/queries/settings";
 import { buildStaticPageMetadata } from "@/lib/metadata";
 import { buildWebsiteJsonLd } from "@/lib/structured-data";
@@ -32,38 +33,18 @@ export async function generateMetadata() {
 }
 
 export default async function HomePage() {
-  const [projects, articles, labs, settings, about] = await Promise.all([
+  const [projects, articles, labs, settings, about, showcaseProjectIds] = await Promise.all([
     getAllProjects(),
     getAllArticles(),
     getAllLabs(),
     getSiteSettings(),
     getAboutPage(),
+    getHomepageShowcaseProjectIds(),
   ]);
 
-  const previewProjects = projects.filter((project) => Boolean(project.frontmatter.liveSiteUrl || project.frontmatter.thumbnail));
-  const automaticOrder = [
-    ...previewProjects,
-    ...projects.filter((project) => !previewProjects.some((preview) => preview.recordId === project.recordId)),
-  ];
-
   const projectById = new Map(projects.map((project) => [project.recordId, project]));
-  const configuredPrimary = settings.homepageProjectIds[0]
-    ? projectById.get(settings.homepageProjectIds[0])
-    : undefined;
-  const configuredSecondary = settings.homepageProjectIds[1]
-    ? projectById.get(settings.homepageProjectIds[1])
-    : undefined;
-
-  const usedProjectIds = new Set<string>();
-  const nextAutomatic = () => automaticOrder.find((project) => !usedProjectIds.has(project.recordId));
-
-  const primaryProject = configuredPrimary ?? nextAutomatic();
-  if (primaryProject) usedProjectIds.add(primaryProject.recordId);
-
-  const secondaryProject = configuredSecondary && !usedProjectIds.has(configuredSecondary.recordId)
-    ? configuredSecondary
-    : nextAutomatic();
-  if (secondaryProject) usedProjectIds.add(secondaryProject.recordId);
+  const primaryProject = showcaseProjectIds[0] ? projectById.get(showcaseProjectIds[0]) : undefined;
+  const secondaryProject = showcaseProjectIds[1] ? projectById.get(showcaseProjectIds[1]) : undefined;
 
   const currentYear = new Date().getFullYear();
   const websiteJsonLd = buildWebsiteJsonLd({ name: settings.name, description: siteConfig.description });
