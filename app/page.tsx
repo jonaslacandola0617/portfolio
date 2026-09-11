932933dd0ef95ba6c42cf059e9f0252e87e44aec
Jonas Lacandola <jonaslacandola07@gmail.com>
2026-09-12 00:20:36 +0800

    feat(home): honor CMS showcase project selection

--- app/page.tsx
+++ app/page.tsx
@@ -41,10 +41,30 @@ export default async function HomePage() {
   ]);
 
   const previewProjects = projects.filter((project) => Boolean(project.frontmatter.liveSiteUrl || project.frontmatter.thumbnail));
-  const orderedProjects = [
+  const automaticOrder = [
     ...previewProjects,
     ...projects.filter((project) => !previewProjects.some((preview) => preview.recordId === project.recordId)),
   ];
+
+  const projectById = new Map(projects.map((project) => [project.recordId, project]));
+  const configuredPrimary = settings.homepageProjectIds[0]
+    ? projectById.get(settings.homepageProjectIds[0])
+    : undefined;
+  const configuredSecondary = settings.homepageProjectIds[1]
+    ? projectById.get(settings.homepageProjectIds[1])
+    : undefined;
+
+  const usedProjectIds = new Set<string>();
+  const nextAutomatic = () => automaticOrder.find((project) => !usedProjectIds.has(project.recordId));
+
+  const primaryProject = configuredPrimary ?? nextAutomatic();
+  if (primaryProject) usedProjectIds.add(primaryProject.recordId);
+
+  const secondaryProject = configuredSecondary && !usedProjectIds.has(configuredSecondary.recordId)
+    ? configuredSecondary
+    : nextAutomatic();
+  if (secondaryProject) usedProjectIds.add(secondaryProject.recordId);
+
   const currentYear = new Date().getFullYear();
   const websiteJsonLd = buildWebsiteJsonLd({ name: settings.name, description: siteConfig.description });
 
@@ -53,8 +73,8 @@ export default async function HomePage() {
       <JsonLd data={websiteJsonLd} />
       <HomeHero settings={settings} learningPhilosophy={about.learningPhilosophy} currentYear={currentYear} />
       <HomeSections
-        primaryProject={orderedProjects[0]}
-        secondaryProject={orderedProjects[1]}
+        primaryProject={primaryProject}
+        secondaryProject={secondaryProject}
         featuredLab={labs[0]}
         articles={articles}
         settings={settings}
